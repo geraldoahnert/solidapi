36e3f3cc1b70c3c8a6f9d774325a215f167c13de
Geraldo Ahnert <geraldoahnert@gmail.com>
2021-09-20 11:16:06 -0300

    feat: start/working solid api project

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from '../IMailProvider';
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: 'fbfca3870b5787',
+        pass: '6e43af8e98171c',
+      },
+    });
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body,
+    });
+  }
+}
